974d3d4c9d26edb74bbefa920eb0cf660d553221
Riko Logwirno <logwirno.teknus@gmail.com>
2024-03-04 23:24:37 +0700

    feat: Add primary color to Button

--- src/commons/atom/Button/index.tsx
+++ src/commons/atom/Button/index.tsx
@@ -4,11 +4,12 @@ import styles from "./button.module.css";
 
 type PropsType = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   label?: string;
+  color?: "primary" | "secondary";
 };
 
 const Button = (props: PropsType) => {
   return (
-    <button className={styles.Rectangle} {...props}>
+    <button className={`${styles.Rectangle} ${props.color === "primary" ? styles.primary : ""}`} {...props}>
       {props.label ? <span className={styles.Search}>{props.label}</span> : props.children}
     </button>
   );
