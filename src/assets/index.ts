5d7ee5712be3ed53c5549ba8f2a4557d55b596a1
Riko Logwirno <logwirno.teknus@gmail.com>
2024-03-04 21:13:07 +0700

    feat: Index Assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export * as LogoImg from "./logo_2024-03-04/logo.png";
+export * as CurrencyIcon from "./currency-dollar_2024-03-04/currency-dollar.png";
+export * as MenuIcon from "./menu_2024-03-04/menu.png";
+export * as SearchIcon from "./search_2024-03-04/search.png";
+export * as XIcon from "./x_2024-03-04/x.png";
