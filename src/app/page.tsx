34d5c00939fc75c2f81c149270051d7e02f0c937
Riko Logwirno <logwirno.teknus@gmail.com>
2024-03-04 23:24:55 +0700

    feat: Add onSearch handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import { LogoImg } from "@/assets";
 import SearchSection from "@/commons/molecule/SearchSection";
 
 const Home = () => {
+  const router = useRouter();
+
+  const handleSubmit = (s: string) => {
+    router.push(`/search/${s}`);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-[30px] py-6 bg">
       <div className="flex flex-1 items-center justify-center">
         <Image className="logo" src={LogoImg} alt="logo" width={72.2} height={83.8} />
       </div>
       <div className="flex w-full">
-        <SearchSection onSearch={(v) => console.log(v)} />
+        <SearchSection onSearch={handleSubmit} />
       </div>
     </main>
   );
