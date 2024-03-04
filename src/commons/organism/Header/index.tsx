21d240a4a74ded29e5a4b531c1ac1bd24efe9a4c
Riko Logwirno <logwirno.teknus@gmail.com>
2024-03-04 23:25:54 +0700

    feat: implement Search modal

--- src/commons/organism/Header/index.tsx
+++ src/commons/organism/Header/index.tsx
@@ -1,28 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import { MenuIcon, SearchIcon } from "@/assets";
 import IconWrapper from "@/commons/atom/IconWrapper";
+import SearchModal from "../SearchModal";
 import styles from "./header.module.css";
 
 const Header = () => {
-  const handleMenuClick = () => {
-    console.log("WIP");
+  const router = useRouter();
+
+  const [searchShow, setSearchShow] = useState(false);
+
+  const handleSubmit = (s: string) => {
+    setSearchShow(false);
+    router.push(`/search/${s}`);
   };
 
   const handleSearchClick = () => {
-    console.log("WIP");
+    setSearchShow(true);
   };
 
   return (
-    <header className={`${styles.bar} flex flex-1 flex-row justify-between items-center`}>
-      <div>
-        <IconWrapper icon={MenuIcon} alt="Menu icon" onClick={handleMenuClick} />
+    <header className={styles.bar}>
+      <div className={`${styles.innerCont} flex flex-1 flex-row justify-between items-center`}>
+        <div>
+          <IconWrapper icon={MenuIcon} alt="Menu icon" onClick={handleSearchClick} />
+        </div>
+        <div>ngmusic</div>
+        <div>
+          <IconWrapper icon={SearchIcon} alt="Search icon" onClick={handleSearchClick} />
+        </div>
       </div>
-      <div>ngmusic</div>
-      <div>
-        <IconWrapper icon={SearchIcon} alt="Search icon" onClick={handleSearchClick} />
-      </div>
+      <SearchModal show={searchShow} onSubmit={handleSubmit} onClose={() => setSearchShow(false)} />
     </header>
   );
 };
